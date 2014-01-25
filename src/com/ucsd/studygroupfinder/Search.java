1fafc9a955650f3a24d9047d08f717f0c274ac51
Alvin See <alvin.see@me.com>
2014-01-25 06:05:22 -0800

    written keyword parser for search field.

--- src/com/ucsd/studygroupfinder/Search.java
+++ src/com/ucsd/studygroupfinder/Search.java
@@ -3,8 +3,10 @@ package com.ucsd.studygroupfinder;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.EditText;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -12,6 +14,9 @@ public class Search extends Activity {
 
 	
 	private Button searchButton, groupButton, logoutButton;
+	private EditText sEdit;
+	
+	public static final String KEYWORD = "KEYWORD";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +45,33 @@ public class Search extends Activity {
 		startActivity(i);
 		
 	}
+	
+	public void mySearchOnClick(View view)
+	{
+		sEdit = (EditText)findViewById(R.id.searchfield);
+		String input = sEdit.getText().toString();
+		
+		Intent i = new Intent(this, MyGroupsActivity.class);
+		
+		if(input.contains("_"))
+			input = input.replace('_', ' ');
+		
+		for(int j = 0;; j++) {
+			if(input.contains(" ")) {
+				i.putExtra(KEYWORD + j, input.substring(0, input.indexOf(" ")) );
+				Log.d("Test first", input.substring(0, input.indexOf(" ")));
+				input = new String(input.substring(1+input.indexOf(" ")));
+			}
+			else {
+				i.putExtra(KEYWORD + j, input.substring(0) );
+				Log.d("Test last", input.substring(0));
+				break;
+			}
+		}
+		startActivity(i);
+		
+
+	}
+	
 }
 	
