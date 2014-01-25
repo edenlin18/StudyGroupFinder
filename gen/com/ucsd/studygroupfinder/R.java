912814f5180b7abfd4bf0f80510a9681cbdf5a32
Alvin See <alvin.see@me.com>
2014-01-25 03:06:44 -0800

    search

--- gen/com/ucsd/studygroupfinder/R.java
+++ gen/com/ucsd/studygroupfinder/R.java
@@ -24,18 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int group=0x7f080001;
+        public static final int logout=0x7f080002;
+        public static final int search=0x7f080004;
+        public static final int searchfield=0x7f080003;
+        public static final int status=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_search=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int search=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int logout=0x7f050005;
+        public static final int my_group=0x7f050004;
+        public static final int search=0x7f050006;
+        public static final int title_activity_search=0x7f050003;
     }
     public static final class style {
         /** 
