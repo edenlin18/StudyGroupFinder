f07dc3b3553c37886dece0d7e687f14c8ad8edf5
edenlin18 <edenlin18@hotmail.com>
2014-01-26 05:14:24 -0800

    GOOD COMMIT!! LOGIN ACTIVE

--- gen/com/ucsd/studygroupfinder/R.java
+++ gen/com/ucsd/studygroupfinder/R.java
@@ -105,34 +105,35 @@ public final class R {
         public static final int search_results=0x7f080006;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05001b;
+        public static final int action_forgot_password=0x7f05001c;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050021;
-        public static final int action_sign_in_short=0x7f050022;
-        public static final int action_sign_up=0x7f050019;
+        public static final int action_sign_in_register=0x7f050022;
+        public static final int action_sign_in_short=0x7f050023;
+        public static final int action_sign_up=0x7f05001a;
         public static final int address=0x7f050016;
         public static final int app_name=0x7f050000;
+        public static final int change_school=0x7f050018;
         public static final int choose_a_school=0x7f050017;
         public static final int connect_test=0x7f05000d;
         public static final int create_or_edit=0x7f050009;
         public static final int description=0x7f05000e;
-        public static final int edit_confirm=0x7f050018;
+        public static final int edit_confirm=0x7f050019;
         public static final int edit_group_test=0x7f05000f;
-        public static final int email=0x7f050023;
-        public static final int emailEmpty=0x7f050029;
-        public static final int error_field_required=0x7f050020;
-        public static final int error_incorrect_password=0x7f05001f;
-        public static final int error_invalid_email=0x7f05001d;
-        public static final int error_invalid_password=0x7f05001e;
+        public static final int email=0x7f050024;
+        public static final int emailEmpty=0x7f05002a;
+        public static final int error_field_required=0x7f050021;
+        public static final int error_incorrect_password=0x7f050020;
+        public static final int error_invalid_email=0x7f05001e;
+        public static final int error_invalid_password=0x7f05001f;
         public static final int group_name=0x7f050010;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f05001c;
+        public static final int login_progress_signing_in=0x7f05001d;
         public static final int logout=0x7f050006;
         public static final int my_group=0x7f050005;
         public static final int my_groups_test=0x7f05000c;
-        public static final int password=0x7f050026;
-        public static final int passwordEmpty=0x7f050028;
-        public static final int prompt_password=0x7f050024;
+        public static final int password=0x7f050027;
+        public static final int passwordEmpty=0x7f050029;
+        public static final int prompt_password=0x7f050025;
         public static final int school=0x7f050015;
         public static final int search=0x7f050007;
         public static final int search_results_test=0x7f050011;
@@ -141,12 +142,13 @@ public final class R {
         public static final int start_time=0x7f050013;
         public static final int subject=0x7f050014;
         public static final int title_activity_edit_group=0x7f050008;
-        public static final int title_activity_group_finder_login=0x7f050025;
-        public static final int title_activity_login=0x7f05001a;
+        public static final int title_activity_group_finder_login=0x7f050026;
+        public static final int title_activity_login=0x7f05001b;
         public static final int title_activity_my_groups=0x7f050003;
         public static final int title_activity_search=0x7f050004;
         public static final int title_activity_search_results=0x7f05000a;
-        public static final int warning=0x7f050027;
+        public static final int title_activity_sign_up=0x7f05002b;
+        public static final int warning=0x7f050028;
     }
     public static final class style {
         /** 
